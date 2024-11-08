dadefa35df458c9844a4fc5199daa04e592e738f
seylu <98249191+seyLu@users.noreply.github.com>
2024-11-08 16:30:43 +0800

    feat (setup.sh): update to source on bashrc instead of bash_aliases

--- setup.sh
+++ setup.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
-readonly _PATH_TO_SCRIPTS="${HOME}/Documents/repos"
+readonly REPO_OWNER="seyLu"
+readonly REPO_NAME="scripts"
+readonly REPO_URL="https://github.com/${REPO_OWNER}/${REPO_NAME}.git"
 
-if [[ ! -d "${_PATH_TO_SCRIPTS}" ]]; then
-    mkdir "${_PATH_TO_SCRIPTS}"
+readonly REPO_PARENT_DIR="${HOME}/Documents/repos"
+readonly REPO_DIR="${REPO_PARENT_DIR}/${REPO_NAME}"
+readonly ENTRY_POINT="${REPO_DIR}/bash/__init__"
+
+if [[ ! -d "${REPO_PARENT_DIR}" ]]; then
+    mkdir "${REPO_PARENT_DIR}"
 fi
 
-cd "${_PATH_TO_SCRIPTS}" || exit
+cd "${REPO_PARENT_DIR}" || exit
 
-git clone https://github.com/seyLu/scripts.git
-
-if [[ ! -f ~/.bash_aliases ]]; then
-    touch ~/.bash_aliases
-    echo "#!/bin/bash" >> ~/.bash_aliases
-    echo >> ~/.bash_aliases
+if [[ -d "${REPO_DIR}/.git" ]]; then
+  echo "Directory '${REPO_DIR}' is a git repository. Pulling latest changes..."
+  cd "${REPO_DIR}"
+  git pull
+else
+  echo "Directory '${REPO_DIR}' is not a git repository. Cloning..."
+  git clone "${REPO_URL}"
 fi
 
-readonly _ENTRY_POINT="${_PATH_TO_SCRIPTS}/scripts/bash/__init__"
 
-echo "# Initialize External Scripts" >> ~/.bash_aliases
-echo '[[ -z "${_SCRIPTS_PATH}" ]] && source "${_ENTRY_POINT}"' >> ~/.bash_aliases
+if [[ ! -f ~/.bashrc ]]; then
+    touch ~/.bashrc
+    echo "#!/bin/bash" >> ~/.bashrc
+fi
 
-# shellcheck source="${_ENTRY_POINT}"
-source "${_ENTRY_POINT}"
-build
-reload
+if ! grep -Fq '[[ -z "${_SCRIPTS_PATH}" ]]' ~/.bashrc; then
+    echo >> ~/.bashrc
+    echo "# Initialize External Scripts" >> ~/.bashrc
+    echo '[[ -z "${_SCRIPTS_PATH}" ]]' "&& source \"${ENTRY_POINT}\"" >> ~/.bashrc
+    # shellcheck source="${ENTRY_POINT}"
+    source "${ENTRY_POINT}"
+    echo "Setup complete! Open a new terminal to start using your custom aliases and scripts."
+else
+    echo "No setup needed. Your custom aliases and scripts are already configured."
+fi
