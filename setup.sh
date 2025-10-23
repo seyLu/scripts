a3795f2cf2b510988f5a4da616b2c198e7c644ff
MJ Ulysses Sabit <98249191+seyLu@users.noreply.github.com>
2025-10-23 19:14:06 +0800

    feat: add log alias

--- setup.sh
+++ setup.sh
@@ -15,29 +15,30 @@ fi
 cd "${REPO_PARENT_DIR}" || exit
 
 if [[ -d "${REPO_DIR}/.git" ]]; then
-  echo "Directory '${REPO_DIR}' is a git repository. Pulling latest changes..."
-  cd "${REPO_DIR}"
-  git pull
+    printf '%s\n' "Directory '${REPO_DIR}' is a git repository. Pulling latest changes..."
+    cd "${REPO_DIR}" || exit
+    git pull
 else
-  echo "Directory '${REPO_DIR}' is not a git repository. Cloning..."
-  git clone "${REPO_URL}"
+    printf '%s\n' "Directory '${REPO_DIR}' is not a git repository. Cloning..."
+    git clone "${REPO_URL}"
 fi
-
 
 if [[ ! -f ~/.bashrc ]]; then
     touch ~/.bashrc
-    echo "#!/bin/bash" >> ~/.bashrc
+    printf '%s\n' "#!/bin/bash" >>~/.bashrc
 fi
 
 if ! grep -Fq '! alias reload &>/dev/null' ~/.bashrc; then
-    echo >> ~/.bashrc
-    echo "# Initialize External Scripts" >> ~/.bashrc
-    echo '! alias reload &>/dev/null' "&& source \"${ENTRY_POINT}\"" >> ~/.bashrc
-    echo "# Enhanced colorful prompt with git branch" >> ~/.bashrc
-    echo 'export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\] \$(parse_git_branch)\[\033[00m\]\n\$ "' >> ~/.bashrc
-    # shellcheck source="${ENTRY_POINT}"
+    {
+        echo
+        printf '%s\n' "# Initialize External Scripts"
+        printf '%s\n' '! alias reload &>/dev/null' "&& source \"${ENTRY_POINT}\""
+        printf '%s\n' "# Enhanced colorful prompt with git branch"
+        printf '%s\n' 'export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\] \$(parse_git_branch)\[\033[00m\]\n\$ "'
+    } >>~/.bashrc
+    # shellcheck source=/dev/null
     source "${ENTRY_POINT}"
-    echo "Setup complete! Open a new terminal to start using your custom aliases and scripts."
+    printf '%s\n' "Setup complete! Open a new terminal to start using your custom aliases and scripts."
 else
-    echo "No setup needed. Your custom aliases and scripts are already configured."
+    printf '%s\n' "No setup needed. Your custom aliases and scripts are already configured."
 fi
