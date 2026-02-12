26897de1e9e6e7a5673fed33647dcb601bf24f9f
MJ Ulysses Sabit <98249191+seyLu@users.noreply.github.com>
2026-02-12 14:45:32 +0800

    feat: remove problematic exported readonly vars

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ if ! grep -Fq '[[ -z "$_SCRIPTS_PATH}" ]] &>/dev/null' ~/.bashrc; then
     {
         echo
         printf '%s\n' "# Initialize External Scripts"
-        printf '%s\n' '[[ -z "$_SCRIPTS_PATH}" ]] &>/dev/null' "&& source \"${ENTRY_POINT}\""
+        printf '%s\n' "source \"${ENTRY_POINT}\""
         printf '%s\n' "# Enhanced colorful prompt with git branch"
         printf '%s\n' 'export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\] \$(parse_git_branch)\[\033[00m\]\n\$ "'
         echo
